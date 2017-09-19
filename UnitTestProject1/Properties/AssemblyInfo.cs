6fc9120496e04aecc5b7deced934ddb3c78b9018
manju <manjupaul1@gmail.com>
2017-09-18 20:43:30 -0400

    First

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d52655c-328d-4c1d-b150-93210ea138e1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static IWebDriver driver;
+
+        public object Browser { get; private set; }
+
+        [AssemblyInitialize]
+        public static void setup(TestContext context)
+        {
+
+            driver = new FirefoxDriver();
+            driver.Url = "http://www.dice.com/";
+
+        }
+        [TestMethod]
+        public void TestDiceHomePage()
+        {
+
+            Assert.IsTrue(driver.PageSource.Contains("Find Tech Jobs"));
+
+        }
+        [TestMethod]
+        public void SearchJob()
+        {
+            var searchBox = driver.FindElement(By.Id("search-field-keyword"));
+            searchBox.SendKeys("Test Automation Engineer");
+            var SearchLocation = driver.FindElement(By.Id("search-field-location"));
+            SearchLocation.SendKeys("Washington,DC");
+            var FindTechJob = driver.FindElement(By.XPath("//*[@id='search-form']/fieldset/div[4]/div/div[1]/button"));
+            FindTechJob.Click();              
+            Assert.IsTrue(driver.PageSource.Contains("Current page is 1"));
+
+        }
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Close();
+           
+        }
+
+
+    }
+}
